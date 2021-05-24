bbe2e2771488857585df48263fdfb343ea927d62
Julian Schubert <informatik.schubert@gmail.com>
2021-05-24 15:27:47 +0200

    TheoInf UB

--- TheoInf/Exercises/Sheet05/Task3/main.py
+++ TheoInf/Exercises/Sheet05/Task3/main.py
@@ -3,40 +3,29 @@
 def is_prime(x):
     return False if x < 2 else all([x % i != 0 for i in range(2, x - 1)])
 
-primes = [x for x in range(1000) if is_prime(x)]
 
-# Printing first 10 primes
-print(primes[:10])
+def get_next_prime(x):
+    x += 1
+    while not is_prime(x):
+        x += 1
+    return x
 
-res = {}
+def h(x, t):
 
-# Testing if the numbers are in B
-for curr in range(20):
+    p = 2 
+    q = 2
 
-    biggest_prime_index = 1
+    for _ in range(t):
+            
+        if p - q == x:
+            return x
 
-    # Searching the biggest prime that does not break the following condingion
-    # p - q <= n (where q is the prime that comes before q)
-    while True:
-        
-        # Preventing out of bounds
-        assert len(primes) > biggest_prime_index 
+        if p == q:
+            q = 2
+            p = get_next_prime(p)
+        else:
+            q += 1
+    return 2
 
-        if (primes[biggest_prime_index] - primes[biggest_prime_index - 1]) > curr:
-            break
-
-        biggest_prime_index += 1
-    
-
-    # Checking all possible combinations
-    for i in range(biggest_prime_index):
-
-        if curr in res.keys():
-            break
-
-        for j in range(biggest_prime_index):
-            if primes[i] - primes[j] == curr:
-                res[curr] = (primes[i], primes[j])
-
-# Printing the result
-print(res)
+for curr in range(3000):
+    print(f" {curr}: {h(curr, 10000)}")
