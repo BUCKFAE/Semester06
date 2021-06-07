178e2179dde32108143731893df3726726826f55
Julian Schubert <informatik.schubert@gmail.com>
2021-06-07 10:01:29 +0200

    TheoInf UB07

---
+++ TheoInf/Exercises/Sheet07/Zusatz2/main.py
@@ -0,0 +1,17 @@
+import random
+
+def counting_sort(x):
+    
+    output = [0] * len(x)
+
+    return output
+
+
+
+if __name__ == "__main__":
+
+    arr = [random.randrange(3) for i in range(5)]
+
+    print(arr)
+
+    print(counting_sort(arr))
