d6000d9f5ae12fe714d1c127405dfc6132833ac9
Julian Schubert <informatik.schubert@gmail.com>
2021-07-12 17:36:27 +0200

    Klausur

--- DataMining/Training/Regression/main.py
+++ DataMining/Training/Regression/main.py
@@ -8,21 +8,22 @@ def main():
     # Data from the example sheet
     data_x, data_y = datasets.make_regression(n_samples=100, n_features=1, noise=5, bias=10)
 
-
     # Initial theta 0 and theta 1
-    t0 = random.random()
-    t1 = random.random()
+    t0 = 0
+    t1 = 0
 
     for iter_no in range(0, 100):
         current_error = (1 / len(data_x)) * sum([(t0 + t1 * data_x[i] - data_y[i]) ** 2 for i in range(len(data_x))])
         print(f"{iter_no=}:{current_error=}")
 
         # Calculating gradient
-        tmp1 = (1 / len(data_x)) * sum([2 * (t0 + t1 * data_x[i] - data_y[i]) for i in range(len(data_x))])
-        tmp2 = (1 / len(data_x)) * sum([2 * data_x[i] * (t0 + t1 * data_x[i] - data_y[i]) for i in range(len(data_x))])
+        tmp1 = (1 / (len(data_x))) * sum([(t0 + t1 * data_x[i] - data_y[i]) for i in range(len(data_x))])
+        tmp2 = (1 / (len(data_x))) * sum([data_x[i] * (t0 + t1 * data_x[i] - data_y[i]) for i in range(len(data_x))])
 
-        t0 -= tmp1 * 0.03
-        t1 -= tmp2 * 0.03
+        t0 -= tmp1 * 0.1
+        t1 -= tmp2 * 0.1
+
+        print(f"t0: {t0}, t1: {t1}")
 
     # Plotting model
     line_x = [x for x in range(int(min(data_x) - 1), int(max(data_x) + 2))]
